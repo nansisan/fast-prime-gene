a6d4dd1f25472199246a61aa8cbf2326405ea60d
nansisan <nansisan@outlook.jp>
2024-07-15 14:44:05 +0900

    Update prime-gene.js

--- prime-gene.js
+++ prime-gene.js
@@ -1,36 +1,34 @@
 function SieveOfAtkin(limit) {
     console.time("Total time");
 
-    console.time("Array generation time");
-    let sieve = new Uint8Array(Math.ceil((limit + 1) / 8));
-    if(limit <= 2**31-1){
-        var primes = new Int32Array(Math.ceil(1.25506 * limit / Math.log(limit))).fill(0);
-        console.log("memory size: " + Math.floor((sieve.length + primes.length * 4) / 1024) + " KB.");
-    } else {
+    if (limit > 2**31 - 1) {
         throw new Error("The upper limit must be less than 2**31-1.");
     }
-    console.timeEnd("Array generation time");
 
-    console.log("Calculation start.");
+    console.time("Array generation time");
+    let sieve = new Uint8Array(Math.ceil((limit + 1) / 8));
+    let primes = new Int32Array(Math.ceil(1.25506 * limit / Math.log(limit))).fill(0);
+    console.log("Memory size: " + Math.floor((sieve.length + primes.length * 4) / 1024) + " KB.");
+    console.timeEnd("Array generation time");
 
     primes[0] = 2;
     primes[1] = 3;
     let index = 2;
 
     console.time("Calculation time");
-    const limitSqrt = Math.sqrt(limit);
+    const limitSqrt = Math.floor(Math.sqrt(limit));
 
     for (let x = 1; x <= limitSqrt; x++) {
         const xSqr = x * x;
         for (let y = 1; y <= limitSqrt; y++) {
             const ySqr = y * y;
 
-            let n = 4 * xSqr + ySqr;
+            let n = (xSqr << 2) + ySqr;//n = 4 * xSqr + ySqr
             if (n <= limit && (n % 12 == 1 || n % 12 == 5)) {
                 sieve[n >> 3] ^= 1 << (7 & n);
             }
 
-            n = 3 * xSqr + ySqr;
+            n =- xSqr;//n = 3 * xSqr + ySqr
             if (n <= limit && n % 12 == 7) {
                 sieve[n >> 3] ^= 1 << (7 & n);
             }
@@ -54,6 +52,7 @@ function SieveOfAtkin(limit) {
             }
         }
     }
+
     for (let n = 5; n < limit; n += 2) {
         if (sieve[n >> 3] & (1 << (7 & n))) {
             primes[index++] = n;
@@ -67,3 +66,5 @@ function SieveOfAtkin(limit) {
 
     return primes.subarray(0, index);
 }
+
+SieveOfAtkin(1e8)
