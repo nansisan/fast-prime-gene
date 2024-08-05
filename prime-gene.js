2fa4aa4861171f0730d7bf21da8e57095930c5ea
nansisan <nansisan@outlook.jp>
2024-08-05 22:34:41 +0900

    Update prime-gene.js

--- prime-gene.js
+++ prime-gene.js
@@ -6,9 +6,9 @@ function SieveOfAtkin(limit) {
     }
 
     console.time("Array generation time");
-    let sieve = new Uint8Array(Math.ceil((limit + 1) / 8));
+    let sieve = new Uint32Array(Math.ceil((limit + 31) / 32));
     let primes = new Int32Array(Math.ceil(1.25506 * limit / Math.log(limit))).fill(0);
-    console.log("Memory size: " + Math.floor((sieve.length + primes.length * 4) / 1024) + " KB.");
+    console.log("Memory size: " + Math.floor((sieve.length * 4 + primes.length * 4) / 1024) + " KB.");
     console.timeEnd("Array generation time");
 
     primes[0] = 2;
@@ -18,43 +18,42 @@ function SieveOfAtkin(limit) {
     console.time("Calculation time");
     const limitSqrt = Math.floor(Math.sqrt(limit));
 
-    for (let x = 1; x <= limitSqrt; x++) {
-        const xSqr = x * x;
-        for (let y = 1; y <= limitSqrt; y++) {
-            const ySqr = y * y;
-
-            let n = (xSqr << 2) + ySqr;//n = 4 * xSqr + ySqr
-            if (n <= limit && (n % 12 == 1 || n % 12 == 5)) {
-                sieve[n >> 3] ^= 1 << (7 & n);
+    for (let x = 1; 4 * x * x <= limit; x++) {
+        for (let y = 1; y <= limitSqrt; y+=2) {
+            let n = 4 * x * x + y * y
+            if (n <= limit && (n % 12 === 1 || n % 12 === 5)) {
+                sieve[n >> 5] ^= 1 << (31 & n);
             }
-
-            n -= xSqr;//n = 3 * xSqr + ySqr
-            if (n <= limit && n % 12 == 7) {
-                sieve[n >> 3] ^= 1 << (7 & n);
+        }
+	}
+	for (let x = 1; 3 * x * x <= limit; x+=2) {
+        for (let y = 2; y <= limitSqrt; y+=2) {
+            n = 3 * x * x + y * y;
+            if (n <= limit && n % 12 === 7) {
+                sieve[n >> 5] ^= 1 << (31 & n);
             }
-
-            if (x > y) {
-                n = 3 * xSqr - ySqr;
-                if (n <= limit && n % 12 == 11) {
-                    sieve[n >> 3] ^= 1 << (7 & n);
-                }
+        }
+	}
+	for (let x = 1; 2 * x * x <= limit; x++) {
+		for (let y = 1 + x & 1; y < x; y+=2) {
+            n = 3 * x * x - y * y;
+            if (n <= limit && n % 12 === 11) {
+                sieve[n >> 5] ^= 1 << (31 & n);
+            }
+		}
+    }
+    for (let n = 5; n <= limitSqrt; n += 2) {
+        if (sieve[n >> 5] & (1 << (31 & n))) {
+            for (let k = n * n; k < limit; k += (n * n) << 1) {
+                sieve[k >> 5] &= ~(1 << (31 & k));
             }
         }
     }
-    console.timeEnd("Calculation time");
+	console.timeEnd("Calculation time");
 
     console.time("Prime number array generation time");
-    for (let n = 5; n <= limitSqrt; n++) {
-        if (sieve[n >> 3] & (1 << (7 & n))) {
-            const nSqr = n * n;
-            for (let k = nSqr; k < limit; k += nSqr) {
-                sieve[k >> 3] &= ~(1 << (7 & k));
-            }
-        }
-    }
-
     for (let n = 5; n < limit; n += 2) {
-        if (sieve[n >> 3] & (1 << (7 & n))) {
+        if (sieve[n >> 5] & (1 << (31 & n))) {
             primes[index++] = n;
         }
     }
@@ -67,4 +66,4 @@ function SieveOfAtkin(limit) {
     return primes.subarray(0, index);
 }
 
-SieveOfAtkin(1e8)
+console.log(SieveOfAtkin(1e8).length);
